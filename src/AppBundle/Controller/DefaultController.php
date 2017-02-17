9c00860dcf3fc932d9995ff9fd8ffea5da96a0a9
Pierrekompf <louisjo10@orange.fr>
2017-02-17 10:11:17 +0100

    Début de Stuff me

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+
+    public function indexAction() {
+        return $this->render("AppBundle:Default:index.html.twig");
+    }
+
+    /**
+     * @Route("/admin", name="admin")
+     */
+
+    public function adminAction(){
+        return $this->render("AppBundle:Default:admin.html.twig");
+    }
+
+    /**
+     * @Route("/admin_user", name="admin_user")
+     */
+
+    public function admin_userAction(){
+        return $this->render("AppBundle:Default:admin_user.html.twig");
+    }
+
+    /**
+     * @Route("/admin_jeu", name="admin_jeu")
+     */
+
+    public function admin_jeuAction(){
+        return $this->render("AppBundle:Default:admin_jeu.html.twig");
+    }
+}
