ac73a2c1e285ab6b47fe2d001bc193ddad9625dc
Pierrekompf <louisjo10@orange.fr>
2017-03-24 11:11:48 +0100

    Admin finit a mettre en ligne

--- src/AppBundle/Entity/stuff_me_user.php
+++ src/AppBundle/Entity/stuff_me_user.php
@@ -23,6 +23,34 @@ class stuff_me_user extends BaseUser
     protected $id;
 
     /**
+    * @var string
+    *
+    * @ORM\Column(name="nationalite", type="string", length=255)
+     */
+    private $nationalite;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="win", type="bigint")
+     */
+    private $win;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="loose", type="bigint")
+     */
+    private $loose;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="totale_score", type="bigint")
+     */
+    private $totale_score;
+
+    /**
      * Get id
      *
      * @return int
@@ -30,5 +58,101 @@ class stuff_me_user extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Set nationalite
+     *
+     * @param string $nationalite
+     *
+     * @return stuff_me_user
+     */
+    public function setNationalite($nationalite)
+    {
+        $this->nationalite = $nationalite;
+
+        return $this;
+    }
+
+    /**
+     * Get nationalite
+     *
+     * @return string
+     */
+    public function getNationalite()
+    {
+        return $this->nationalite;
+    }
+
+    /**
+     * Set win
+     *
+     * @param integer $win
+     *
+     * @return stuff_me_user
+     */
+    public function setWin($win)
+    {
+        $this->win = $win;
+
+        return $this;
+    }
+
+    /**
+     * Get win
+     *
+     * @return integer
+     */
+    public function getWin()
+    {
+        return $this->win;
+    }
+
+    /**
+     * Set loose
+     *
+     * @param integer $loose
+     *
+     * @return stuff_me_user
+     */
+    public function setLoose($loose)
+    {
+        $this->loose = $loose;
+
+        return $this;
+    }
+
+    /**
+     * Get loose
+     *
+     * @return integer
+     */
+    public function getLoose()
+    {
+        return $this->loose;
+    }
+
+    /**
+     * Set totaleScore
+     *
+     * @param integer $totaleScore
+     *
+     * @return stuff_me_user
+     */
+    public function setTotaleScore($totaleScore)
+    {
+        $this->totale_score = $totaleScore;
+
+        return $this;
+    }
+
+    /**
+     * Get totaleScore
+     *
+     * @return integer
+     */
+    public function getTotaleScore()
+    {
+        return $this->totale_score;
     }
 }
